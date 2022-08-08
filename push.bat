9b8b39a5c384daf03a7796cea2ad599361b18feb
winterrcold <2746763180@qq.com>
2022-08-08 22:34:52 +0800

    hexobackup

--- push.bat
+++
@@ -1,8 +0,0 @@
-cd /d  C:\Users\Parsifal\Desktop\blog
-title HexoGoGoGo
-cd 
-call hexo clean
-call hexo g
-call hexo d
-echo
-pause
--- writing.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set /p name=input pages name:
-echo please wait
-cd C:\Users\Parsifal\Desktop\blog
-call hexo new null %name%
-start C:\Users\Parsifal\Desktop\blog\source\_posts\%name%.md
-pause
