cb7998bd21e922d58209d3eb49bf0026ebc2fbd4
Parsifa1 <2746763180@qq.com>
2023-07-13 19:55:00 +0800

    hexobackup

--- push.bat
+++ push.bat
@@ -3,6 +3,7 @@ title HexoGoGoGo
 cd 
 call git config --global user.email "2746763180@qq.com"
 call git config --global user.name "Parsifa1"
+git config --global core.autocrlf false 
 cls
 call hexo clean
 call hexo g
