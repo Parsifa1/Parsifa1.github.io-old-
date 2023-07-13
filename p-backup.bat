fc671c992e2dbd41ea0a9113638c80ed8aa0596d
Parsifa1 <2746763180@qq.com>
2023-07-13 19:50:42 +0800

    hexobackup

--- p-backup.bat
+++ p-backup.bat
@@ -1,5 +1,7 @@
 @echo off
 cd D:\Aldric\desktop\blog
+call git config --global user.email "2746763180@qq.com"
+call git config --global user.name "Parsifa1"
 git add .
 git commit -m "hexobackup"
 git push
