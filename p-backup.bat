aa167c9ac3a685fc16f4e7b3d5f30aab05751530
Parsifa1 <2746763180@qq.com>
2023-11-28 18:53:39 +0800

    删除sass，避免报错

--- p-backup.bat
+++ p-backup.bat
@@ -3,6 +3,8 @@ cd E:\Aldric\desktop\blog
 call git config --global user.email "2746763180@qq.com"
 call git config --global user.name "Parsifa1"
 git add .
-git commit -m "hexobackup"
+set /p name=input commit:
+echo please wait
+git commit -m "%name%"
 git push
 pause
